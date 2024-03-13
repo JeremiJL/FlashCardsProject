4cd7967117a8d94f54f986a3f7021e4de3b06ac7
JeremiJL <j3remilipniacki@gmail.com>
2024-03-13 12:39:23 +0100

    Initial Commit of my FlashCards Project

---
+++ src/main/java/jeremijl/flashcardsproject/FlashCardsProjectApplication.java
@@ -0,0 +1,13 @@
+package jeremijl.flashcardsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashCardsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashCardsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jeremijl/flashcardsproject/FlashCardsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package jeremijl.flashcardsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashCardsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
